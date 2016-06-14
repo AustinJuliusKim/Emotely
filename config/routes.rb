788349e05592463fe06a70c95f5807d0449f32f0
austinjuliuskim <austinjuliuskim@gmail.com>
2016-06-14 13:24:55 -0700

    Add users and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  resources :users
+  root 'emotes#index'
+  get 'signup' => 'users#new'
   resources :emotes
+  #routes for logging in/out
+  get 'login/' => 'sessions#new'
+  post 'login/' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
